4534d8442defb7282e7be34e613845bed8cac912
Saehanseul <saehanseul@gmail.com>
2024-08-26 12:25:55 +0900

    Etc: 스타일 색상 추가

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,5 +1,9 @@
 export const colors = {
   purple_primary: "#5D54B7",
   purple_background: "#F1F0FF",
-  purple_background_active: "#E0DDFF"
+  purple_background_active: "#E0DDFF",
+
+  white: "#FFFFFF",
+  grey: "#9EA8AA",
+  black: "#000000"
 };
