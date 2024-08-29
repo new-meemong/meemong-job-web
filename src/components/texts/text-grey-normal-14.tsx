17bcd47c7d95ce9bee982df12fdf6e0bd3a27f58
Saehanseul <saehanseul@gmail.com>
2024-08-29 13:38:32 +0900

    Etc: 누락된 텍스트 추가

---
+++ src/components/texts/text-grey-normal-14.tsx
@@ -0,0 +1,10 @@
+import { colors } from "@/styles/styles";
+import styled from "styled-components";
+
+const TextGreyNormal14 = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  color: ${colors.grey};
+`;
+
+export default TextGreyNormal14;
