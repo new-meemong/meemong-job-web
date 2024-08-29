6c59687c297e01c58cf850b70ab6954290b12206
Saehanseul <saehanseul@gmail.com>
2024-08-29 09:30:39 +0900

    Feat: full button 추가

---
+++ src/components/buttons/full-button.tsx
@@ -0,0 +1,29 @@
+import { colors } from "@/styles/styles";
+import styled from "styled-components";
+import TextWhiteBold16 from "../texts/text-white-bold-16";
+
+const Container = styled.div`
+  background-color: ${colors.purple_primary};
+  height: 48px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled(TextWhiteBold16)``;
+
+interface TitleProps {
+  title: string;
+  onClick: () => void;
+}
+
+const FullButton = ({ title, onClick }: TitleProps) => {
+  return (
+    <Container onClick={onClick}>
+      <Title>{title}</Title>
+    </Container>
+  );
+};
+
+export default FullButton;
