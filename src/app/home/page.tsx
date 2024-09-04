6bc668585913de2f7c501103ff845449e7b88d30
Saehanseul <saehanseul@gmail.com>
2024-09-04 15:09:10 +0900

    Refactor: home/page import 정리

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { colors } from "@/styles/colors";
 import { useState } from "react";
 import styled from "styled-components";
 import BaseTopTabs from "./components/base-top-tab";
 import FloatingButton from "./components/floating-button";
-import FindDesignerRequiredFilter from "./components/sections/find-designer/find-designer-required-filter";
 import HomeTitle from "./components/home-title";
-import TalentSearchButton from "./components/sections/find-designer/talent-search-button";
-import Banner from "./components/banner";
-import HomeSearchResultList from "./components/sections/find-designer/home-designer-search-list";
-import pxToVw from "@/lib/dpi-converter";
 import FindDesignerSection from "./components/sections/find-designer/find-designer-section";
 import FindJobSection from "./components/sections/find-job/find-job-section";
 
