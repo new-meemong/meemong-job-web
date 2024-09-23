65d18cffbd3db224ce5143e9279e1e33f0f3b656
Saehanseul <saehanseul@gmail.com>
2024-09-23 12:18:44 +0900

    Feat: 시간 선택 추가

--- src/app/job-posting/[id]/edit/components/etc-option-components/input-work-time.tsx
+++ src/app/job-posting/[id]/edit/components/etc-option-components/input-work-time.tsx
@@ -1,6 +1,8 @@
 import pxToVw from "@/lib/dpi-converter";
 import { useJobPostingEditStore } from "@/stores/job-posting-edit-store";
 import { fonts } from "@/styles/fonts";
+import { useState } from "react";
+import { Sheet } from "react-modal-sheet";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -14,6 +16,12 @@ const Label = styled.div`
   padding: ${pxToVw(8)} 0;
   width: ${pxToVw(120)};
   flex-shrink: 0;
+`;
+
+const WorkTimeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const WorkTimeInput = styled.div<{ $isEmpty: boolean }>`
@@ -35,14 +43,134 @@ const WorkTimeInput = styled.div<{ $isEmpty: boolean }>`
     props.$isEmpty ? fonts.greyPlaceholderBold14 : fonts.purplePrimaryBold14};
 `;
 
+const SheetContainer = styled(Sheet.Container)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-top-right-radius: ${pxToVw(30)} !important;
+  border-top-left-radius: ${pxToVw(30)} !important;
+`;
+
+const SheetHeader = styled(Sheet.Header)``;
+
+const SheetContent = styled(Sheet.Content)`
+  height: 100%;
+  padding-left: ${pxToVw(24)};
+  padding-right: ${pxToVw(24)};
+`;
+
+const TimeOption = styled.div`
+  padding: ${pxToVw(10)} 0;
+  cursor: pointer;
+  text-align: center;
+  ${fonts.blackSemi14};
+`;
+
 const InputWorkTime = () => {
   const { startWorkTime, endWorkTime, setStartWorkTime, setEndWorkTime } =
     useJobPostingEditStore();
+  const [isStartTimeModalOpen, setIsStartTimeModalOpen] = useState(false);
+  const [isEndTimeModalOpen, setIsEndTimeModalOpen] = useState(false);
+
+  const generateTimeOptions = () => {
+    const times = [];
+    for (let hour = 0; hour <= 24; hour++) {
+      for (let minute = 0; minute < 60; minute += 30) {
+        if (hour === 24 && minute > 0) break; // 24:30은 없으므로 break
+        const formattedHour = String(hour).padStart(2, "0");
+        const formattedMinute = String(minute).padStart(2, "0");
+        times.push(`${formattedHour}:${formattedMinute}`);
+      }
+    }
+    return times;
+  };
+
+  const handleOpenStartTimeModal = () => {
+    setIsStartTimeModalOpen(true);
+  };
+
+  const handleOpenEndTimeModal = () => {
+    setIsEndTimeModalOpen(true);
+  };
+
+  const handleCloseStartTimeModal = () => {
+    setIsStartTimeModalOpen(false);
+  };
+
+  const handleCloseEndTimeModal = () => {
+    setIsEndTimeModalOpen(false);
+  };
+
+  const handleStartTimeSelect = (time: string) => {
+    setStartWorkTime(time); // 선택한 시간을 startWorkTime에 반영
+    handleCloseStartTimeModal(); // 모달 닫기
+  };
+
+  const handleEndTimeSelect = (time: string) => {
+    setEndWorkTime(time); // 선택한 시간을 endWorkTime에 반영
+    handleCloseEndTimeModal(); // 모달 닫기
+  };
 
   return (
     <Container>
       <Label>근무 시간</Label>
-      <WorkTimeInput $isEmpty={true}>00:00~00:00</WorkTimeInput>
+      <WorkTimeWrapper>
+        <WorkTimeInput
+          $isEmpty={!startWorkTime}
+          onClick={handleOpenStartTimeModal}
+        >
+          {startWorkTime || "00:00"}
+        </WorkTimeInput>
+        <div> ~ </div>
+        <WorkTimeInput $isEmpty={!endWorkTime} onClick={handleOpenEndTimeModal}>
+          {endWorkTime || "00:00"}
+        </WorkTimeInput>
+      </WorkTimeWrapper>
+      <Sheet
+        isOpen={isStartTimeModalOpen}
+        onClose={handleCloseStartTimeModal}
+        snapPoints={[0.6]}
+      >
+        <Sheet.Backdrop onTap={handleCloseStartTimeModal} />
+        <SheetContainer>
+          <SheetHeader />
+          <SheetContent>
+            <Sheet.Scroller>
+              {generateTimeOptions().map((time) => (
+                <TimeOption
+                  key={time}
+                  onClick={() => handleStartTimeSelect(time)}
+                >
+                  {time}
+                </TimeOption>
+              ))}
+            </Sheet.Scroller>
+          </SheetContent>
+        </SheetContainer>
+      </Sheet>
+      <Sheet
+        isOpen={isEndTimeModalOpen}
+        onClose={handleCloseEndTimeModal}
+        snapPoints={[0.6]}
+      >
+        <Sheet.Backdrop onTap={handleCloseEndTimeModal} />
+        <SheetContainer>
+          <SheetHeader />
+          <SheetContent>
+            <Sheet.Scroller>
+              {generateTimeOptions().map((time) => (
+                <TimeOption
+                  key={time}
+                  onClick={() => handleEndTimeSelect(time)}
+                >
+                  {time}
+                </TimeOption>
+              ))}
+            </Sheet.Scroller>
+          </SheetContent>
+        </SheetContainer>
+      </Sheet>
     </Container>
   );
 };
