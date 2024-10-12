d86bca98edaeebad7399198c61bfa703689a5ed1
Saehanseul <saehanseul@gmail.com>
2024-10-12 16:53:37 +0900

    Etc: viewport zoom in 방지 테스트 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,10 @@ export default function RootLayout({
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         /> */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </head>
       <body style={{ margin: 0 }}>
         <StyledComponentsRegistry>
