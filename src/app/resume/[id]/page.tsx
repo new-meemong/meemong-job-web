e6cb5b46ce5167f0e4bce88800d499fa119420b2
Saehanseul <saehanseul@gmail.com>
2024-10-23 14:48:52 +0900

    test: meta

--- src/app/resume/[id]/page.tsx
+++ src/app/resume/[id]/page.tsx
@@ -74,6 +74,7 @@ export default function ResumePage() {
     return (
       <>
         <Metadata
+          ogUrl={`https://meemong.com/resumes/${resumeId}`}
           ogImage="https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/resumes/profiles/2024/10/15/images/9dbf494e-70b4-4e2e-8fef-3e31fb998a26/s1024/9dbf494e-70b4-4e2e-8fef-3e31fb998a26.jpg"
           ogTitle={"로딩 메타데이터"}
         />
@@ -86,6 +87,7 @@ export default function ResumePage() {
     return (
       <>
         <Metadata
+          ogUrl={`https://meemong.com/resumes/${resumeId}`}
           ogImage="https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/resumes/profiles/2024/10/15/images/9dbf494e-70b4-4e2e-8fef-3e31fb998a26/s1024/9dbf494e-70b4-4e2e-8fef-3e31fb998a26.jpg"
           ogTitle={"없음 메타데이터"}
         />
