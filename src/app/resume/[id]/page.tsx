3c0da53765e4b09ea2b883cde27c9d0b472ddb7c
Saehanseul <saehanseul@gmail.com>
2024-10-23 14:45:49 +0900

    test: meta

--- src/app/resume/[id]/page.tsx
+++ src/app/resume/[id]/page.tsx
@@ -71,11 +71,27 @@ export default function ResumePage() {
   }, [resumeId]);
 
   if (isLoading) {
-    return <CenterSpinner />;
+    return (
+      <>
+        <Metadata
+          ogImage="https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/resumes/profiles/2024/10/15/images/9dbf494e-70b4-4e2e-8fef-3e31fb998a26/s1024/9dbf494e-70b4-4e2e-8fef-3e31fb998a26.jpg"
+          ogTitle={"로딩 메타데이터"}
+        />
+        <CenterSpinner />
+      </>
+    );
   }
 
   if (!resume) {
-    return <div>존재하지 않는 이력서입니다.</div>;
+    return (
+      <>
+        <Metadata
+          ogImage="https://meemong-job-storage.s3.ap-northeast-2.amazonaws.com/uploads/resumes/profiles/2024/10/15/images/9dbf494e-70b4-4e2e-8fef-3e31fb998a26/s1024/9dbf494e-70b4-4e2e-8fef-3e31fb998a26.jpg"
+          ogTitle={"없음 메타데이터"}
+        />
+        <div>존재하지 않는 이력서입니다.</div>;
+      </>
+    );
   }
   return (
     <Container>
