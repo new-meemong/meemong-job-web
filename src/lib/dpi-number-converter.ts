136a47217f057df89fd21ab43ec2736e0c8f74fa
Saehanseul <saehanseul@gmail.com>
2024-10-30 13:04:28 +0900

    refactor: add dp converter max 600px limit

--- src/lib/dpi-number-converter.ts
+++ src/lib/dpi-number-converter.ts
@@ -2,7 +2,7 @@ const numberToVw = (value: number): number => {
   if (typeof window === "undefined") {
     return value; // 서버에서 실행될 때는 변환하지 않고 원래 값을 반환
   }
-  const viewportWidth = window.innerWidth;
+  const viewportWidth = Math.min(window.innerWidth, 600);
   return (value / 390) * viewportWidth; // 390px 기준으로 vw 변환
 };
 
