5432a5f883193d8ebff74de26eba65b75f98d950
Saehanseul <saehanseul@gmail.com>
2024-11-11 19:05:08 +0900

    refactor: 어플 다운 ui 변경

--- src/components/buttons/store-floating-button.tsx
+++ src/components/buttons/store-floating-button.tsx
@@ -3,6 +3,7 @@ import { colors } from "@/styles/colors";
 import { fonts } from "@/styles/fonts";
 import pxToVw from "@/lib/dpi-converter";
 import styled from "styled-components";
+import toast from "react-hot-toast";
 
 const Container = styled.div`
   display: flex;
@@ -17,6 +18,8 @@ const Container = styled.div`
   padding-top: ${pxToVw(12)};
   padding-bottom: ${pxToVw(12)};
   background-color: ${colors.white};
+  padding-left: ${pxToVw(24)};
+  padding-right: ${pxToVw(24)};
 `;
 
 const StoreContainer = styled.div`
@@ -27,18 +30,26 @@ const StoreContainer = styled.div`
 `;
 
 const PlayStoreButton = styled.div`
-  width: ${pxToVw(128)};
-  height: ${pxToVw(38)};
+  width: ${pxToVw(168)};
+  height: ${pxToVw(41)};
   position: relative;
 `;
 const AppStoreButton = styled.div`
-  width: ${pxToVw(128)};
-  height: ${pxToVw(38)};
+  width: ${pxToVw(168)};
+  height: ${pxToVw(41)};
   position: relative;
 `;
 
 const Title = styled.div`
-  ${fonts.greySemi14}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${fonts.whiteBold16}
+  height: ${pxToVw(48)};
+  background-color: ${colors.purplePrimary};
+  width: 100%;
+  max-width: ${pxToVw(343)};
+  border-radius: ${pxToVw(4)};
 `;
 
 const StoreFloatingButton = ({ title }: { title: string }) => {
@@ -58,7 +69,13 @@ const StoreFloatingButton = ({ title }: { title: string }) => {
 
   return (
     <Container>
-      <Title>{title}</Title>
+      <Title
+        onClick={() => {
+          toast("아래 스토어에서 다운받아 주세요.");
+        }}
+      >
+        {title}
+      </Title>
       <StoreContainer>
         <PlayStoreButton onClick={handlePlayStoreButtonClick}>
           <Image src="/images/playstore_download.svg" alt="Play Store" fill />
