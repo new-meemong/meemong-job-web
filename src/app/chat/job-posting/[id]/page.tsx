7f50a9ee42ab93b88fc8b529117522264bd249ab
Saehanseul <saehanseul@gmail.com>
2024-11-12 17:06:43 +0900

    feat: 페이지 생성중

---
+++ src/app/chat/job-posting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+export default function JobPostingChatDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <div>JobPostingChatDetailPage</div>;
+}
